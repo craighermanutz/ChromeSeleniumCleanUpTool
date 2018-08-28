0e61025d9f89493b5e302f3ef24b1387b37cb354
craighermanutz <42555224+craighermanutz@users.noreply.github.com>
2018-08-28 15:38:26 -0500

    Add files via upload

---
+++ ChromeSeleniumCleanUp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ChromeSeleniumCleanUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chromedrivers = Process.GetProcessesByName("chromedriver");
+            var consoleHosts = Process.GetProcessesByName("chrome.exe");
+            foreach (var process in chromedrivers)
+            {
+                process.Kill();
+            }
+
+            foreach (var process in consoleHosts)
+            {
+                process.Kill();
+            }
+        }
+}
